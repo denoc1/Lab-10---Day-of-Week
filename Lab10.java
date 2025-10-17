9231dc8cd4d3781620228f306f82d0645599cbd4
denoc1 <176614645+denoc1@users.noreply.github.com>
2025-10-17 03:01:56 +0000

    template

---
+++ Lab10.java
@@ -0,0 +1,19 @@
+public class Lab10{
+
+    //dayOfWeek method – given a date, returns the day of week
+    //precondition:  int m(month), int d (day), int y (year) all must be greater than 0; month less than 13, day less than 31, year a 4 digit year.
+    //postcondition:  returns the day of the week (String)
+
+    public static String dayOfWeek(int m, int d, int y)
+    {
+    	// fill in code
+    }
+
+    //main method - should ask user for date;  Prints out final statement.
+    public static void main(String[] args){
+
+
+
+    }
+
+}
